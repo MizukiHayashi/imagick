2eb99fb20b09abf9d2418f0cbdef59159f6de05e
MizukiHayashi <hisekiou2@gmail.com>
2018-05-28 15:04:09 +0900

    #3 とりあえずベースのコードを書いた

--- string.php
+++ string.php
@@ -1,3 +1,17 @@
 <?php
-echo "テスト";
- ?>
+$str = $_GET['str']
+$file = $_GET['file'];
+
+$image = new Imagick($file);
+
+$draw = new ImagickDraw();
+$draw->setFont('/usr/share/fonts/ipa-gothic/ipag.ttf');  //フォントファイル
+$draw->setFontSize('24');                    // フォントサイズ
+$draw->setFillColor('#ff0000');              // 文字色
+$draw->annotation(10, 100, $str);
+$image->drawImage($draw);
+
+header("Content-Type: image/png");
+ob_clean();
+echo $image;
+?>
